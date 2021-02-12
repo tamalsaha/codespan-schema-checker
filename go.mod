757697adc15d29ce9327f55228c8adfb399c14a9
Tamal Saha <tamal@appscode.com>
2021-02-11 16:04:44 -0800

    added-all

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tamalsaha/doc-code-sample-checker
+
+go 1.15
+
+require github.com/yuin/goldmark v1.3.2
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+import (
+	"bytes"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
+)
+
+func main() {
+	md := goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+			html.WithXHTML(),
+		),
+	)
+
+	var buf bytes.Buffer
+	if err := md.Convert([]byte(sample), &buf); err != nil {
+		panic(err)
+	}
+}
